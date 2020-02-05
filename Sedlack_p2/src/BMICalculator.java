1a62743d1b915e290f304399eb9a115436f61dc6
Michael Sedlack <michael.sedlack@knights.ucf.edu>
2020-02-04 20:09:49 -0500

    More closely follows the rubric, resulting in much cleaner code.

--- Sedlack_p2/src/BMICalculator.java
+++ Sedlack_p2/src/BMICalculator.java
@@ -1,54 +1,104 @@
 import java.util.Scanner;
 
 public class BMICalculator {
-	float BMI, height, weight;
-	String category;
-	boolean metric = false;
-	public void readUserData()
+	private Scanner in = new Scanner(System.in);
+	float bmi, height, weight;
+	int unitType;
+	String category, unitTypeHeight, unitTypeWeight;
+	
+	public static void main(String[] args)
 	{
-		String unit;
-		Scanner userInput = new Scanner(System.in);
-		System.out.println("Are you using Kilos or Pounds?");
-		unit=userInput.nextLine();
-		if(unit.equals("kilos")||unit.equals("Kilos"))
-		{
-			metric = true;
-			System.out.println("What is your weight in kilos?");
-			weight = userInput.nextFloat();
-			System.out.println("What is your height in meters?");
-			height = userInput.nextFloat();
-		}
-		else if(unit.equals("Pounds")||unit.equals("pounds"))
-		{
-			metric = false;
-			System.out.println("What is your weight in pounds?");
-			weight = userInput.nextFloat();
-			System.out.println("What is your height in inches?");
-			height = userInput.nextFloat();
-		}
+		BMICalculator app = new BMICalculator();
+	    app.readUserData();
+	    app.calculateBmi();
+	    app.displayBmi();
 	}
-	public void calculateBmi()
-	{
-		if(metric)
-		{
-			BMI = (weight)/(height*height);
-		}
+	
+	public void readUserData() {
+		readUnitType();
+		readMeasurementData();
+	}
+	private void readUnitType()	{
+		do {
+		System.out.println("Please select a measurement type. Choose 0 for metric and 1 for imperial.");
+		unitType = in.nextInt();
+		} while(unitType != 0 && unitType != 1);
+	}
+	private void readMeasurementData() {
+		if(unitType == 0)
+			readMetricData();
 		else
-		{
-			BMI = (703*weight)/(height*height);
-		}
-		
-		if(BMI < 18.5)
-			category = "Underweight";
-		else if(BMI > 18.5 && BMI < 24.9)
-			category = "Normal Weight";
-		else if(BMI >24.9 && BMI < 29.9)
-			category = "Overweight";
-		else if(BMI > 30)
-			category = "Obesity";
+			readImperialData();
 	}
+	private void readImperialData() {
+		unitTypeHeight = "inches";
+		unitTypeWeight = "pounds";
+		setWeight();
+		setHeight();
+	}
+	
+	private void readMetricData(){
+		unitTypeHeight = "meters";
+		unitTypeWeight = "kilograms";
+		setWeight();
+		setHeight();
+	}
+	
+	private void calculateBmi() {
+		if(unitType == 0)
+			bmi = getWeight()/(getHeight()*getHeight());
+		else
+			bmi = (703 * getWeight()/(getHeight()*getHeight()));
+		calculateBmiCategory();
+	}
+	
+	private void calculateBmiCategory() {
+		if(getBmi() >= 30)
+			category = "Obese";
+		else if(getBmi() < 30 && getBmi() >= 25)
+			category = "Overweight";
+		else if(getBmi() < 25 && getBmi() >= 18.5)
+			category = "Normal weight";
+		else if(getBmi() < 18.5)
+			category = "Underweight";
+	}
+	
+	private void setWeight() {
+		System.out.println("Please type in your weight in "+unitTypeWeight+".");
+		weight = in.nextFloat();
+		
+		//error out
+		if(weight < 0)
+			System.exit(0);
+	}
+	
+	public float getWeight() {
+		return weight;
+	}
+
+	private void setHeight() {
+		System.out.println("Please type in your height in "+unitTypeHeight+".");
+		height = in.nextFloat();
+		
+		//error out
+		if(height < 0)
+			System.exit(0);
+	}
+	
+	public float getHeight() {
+		return height;
+	}
+	
+	public float getBmi() {
+		return bmi;
+	}
+	
+	public String getBmiCategory() {
+		return category;
+	}
+	
 	public void displayBmi()
 	{
-		System.out.printf("Your BMI is %.1f. You are categorized as %s", BMI, category);
+		System.out.printf("Your BMI is %.1f. You are categorized as %s", getBmi(), getBmiCategory());
 	}
 }
